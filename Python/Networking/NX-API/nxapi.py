93fe4e8b511ad38c6be6094b695c86a7fa29b4d8
Knox Hutchinson <47797909+DataKnox@users.noreply.github.com>
2019-08-23 11:16:24 -0500

    NXAPI CLI example

---
+++ Python/Networking/NX-API/nxapi.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from pprint import pprint
+
+url = "https://sbx-nxos-mgmt.cisco.com/ins"
+switchuser = "admin"
+switchpassword = "Admin_1234!"
+
+myheaders = {"content-type": "application/json"}
+payload = {
+    "ins_api": {
+        "version": "1.0",
+        "type": "cli_show",
+        "chunk": "0",
+        "sid": "1",
+        "input": "show ip int brief",
+        "output_format": "json",
+    }
+}
+response = requests.post(
+    url,
+    data=json.dumps(payload),
+    headers=myheaders,
+    auth=(switchuser, switchpassword),
+    verify=False,
+).json()
+
+print(json.dumps(response, indent=2, sort_keys=True))
