b9fa20f2c6ec50131b19314a8f4bbcc171576367
Knox Hutchinson <47797909+DataKnox@users.noreply.github.com>
2019-09-09 15:06:34 -0500

    Adding ACI

---
+++ Python/Networking/ACI/ACI Requests.py
@@ -0,0 +1,56 @@
+import requests
+import json
+
+url = "https://sandboxapicdc.cisco.com:443/api/aaaLogin.json"
+
+payload = {
+    "aaaUser": {
+        "attributes": {
+            "name": "admin",
+            "pwd": "ciscopsdt"
+        }
+    }
+}
+headers = {
+    'Content-Type': "application/json"
+}
+
+response = requests.post(url, data=json.dumps(
+    payload), headers=headers, verify=False).json()
+
+print(json.dumps(response, indent=2, sort_keys=True))
+
+# PARSE TOKEN AND SET COOKIE
+token = response['imdata'][0]['aaaLogin']['attributes']['token']
+cookie = {}
+cookie['APIC-cookie'] = token
+
+# GET APPLICATION PROFILE
+url = "https://sandboxapicdc.cisco.com:443/api/node/mo/uni/tn-Heroes/ap-Save_The_Planet.json"
+
+headers = {
+    'cache-control': "no-cache"
+}
+
+get_response = requests.get(
+    url, headers=headers, cookies=cookie, verify=False).json()
+
+print(json.dumps(get_response, indent=2, sort_keys=True))
+
+# SET DESCRIPTION
+post_payload = {
+    "fvAp": {
+        "attributes": {
+            "descr": "",
+            "dn": "uni/tn-Heroes/ap-Save_The_Planet"
+        }
+    }
+}
+
+post_response = requests.post(
+    url, headers=headers, cookies=cookie, verify=False, data=json.dumps(post_payload)).json()
+
+get_response = requests.get(
+    url, headers=headers, cookies=cookie, verify=False).json()
+
+print(json.dumps(get_response, indent=2, sort_keys=True))
