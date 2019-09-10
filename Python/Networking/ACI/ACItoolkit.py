d77a71fe46b98f0bae961c6e73a05e72d425c9a8
Knox Hutchinson <knox@knoxsdata.com>
2019-09-10 08:41:01 -0500

    update acitoolkit

--- Python/Networking/ACI/ACItoolkit.py
+++ Python/Networking/ACI/ACItoolkit.py
@@ -21,8 +21,26 @@ for tenant in tenants:
     print('*' * 30)
     print(' ')
 
-new_tenant = Tenant("Tenant Name Here")
+new_tenant = Tenant("Tenant_Name_Here")
 new_tenant.get_url()
 new_tenant.get_json()
+print(new_tenant.get_url())
+print(new_tenant.get_json())
 
+response = session.push_to_apic(
+    new_tenant.get_url(), data=new_tenant.get_json())
+print(response)
+
+tenants = Tenant.get(session)
+for tenant in tenants:
+    if tenant.name == 'Tenant_Name_Here':
+        print(tenant.name)
+    else:
+        print(tenant.name)
+        print(tenant.descr)
+        print('*' * 30)
+        print(' ')
+
+
+new_tenant.mark_as_deleted()
 session.push_to_apic(new_tenant.get_url(), new_tenant.get_json())
