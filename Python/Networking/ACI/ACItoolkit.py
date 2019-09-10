fdd99759a71e994fad5a5587ee64ea72b3b04a13
Knox Hutchinson <knox@knoxsdata.com>
2019-09-10 08:53:55 -0500

    create full tenant ACItoolkit

--- Python/Networking/ACI/ACItoolkit.py
+++ Python/Networking/ACI/ACItoolkit.py
@@ -21,12 +21,27 @@ for tenant in tenants:
     print('*' * 30)
     print(' ')
 
+# Create a new Tenant
 new_tenant = Tenant("Tenant_Name_Here")
 new_tenant.get_url()
 new_tenant.get_json()
+
+
+# Create the application profile and the EPG
+anp = AppProfile('Knoxs_app', new_tenant)
+epg = EPG('Knoxs_epg', anp)
+
+# Create the L3 Namespace
+context = Context('Knoxs_VRF', new_tenant)
+bridge_domain = BridgeDomain('Knoxs_bd', new_tenant)
+
+# Associate the BD with the L3 Namespace
+bridge_domain.add_context(context)
+epg.add_bd(bridge_domain)
+
+##### Tenant Creation is completed #####
 print(new_tenant.get_url())
 print(new_tenant.get_json())
-
 response = session.push_to_apic(
     new_tenant.get_url(), data=new_tenant.get_json())
 print(response)
@@ -41,6 +56,5 @@ for tenant in tenants:
         print('*' * 30)
         print(' ')
 
-
-new_tenant.mark_as_deleted()
-session.push_to_apic(new_tenant.get_url(), new_tenant.get_json())
+# new_tenant.mark_as_deleted()
+#session.push_to_apic(new_tenant.get_url(), new_tenant.get_json())
