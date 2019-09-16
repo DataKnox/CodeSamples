ddf2de94e375d52b3dd306cf29cd77acda0680c8
Knox Hutchinson <47797909+DataKnox@users.noreply.github.com>
2019-09-16 11:04:09 -0500

    Added token URL comment

--- Python/Networking/Collaboration/webex.py
+++ Python/Networking/Collaboration/webex.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+# Get your token here after logging in: https://developer.webex.com/docs/api/getting-started
 token = 'MjgyYzQyYjEtNTNmOS00NTQ0LWEyNjMtZDA5OWU2NjIxYjFkZjdiOGJjMzMtNjQ4_PF84_75e84279-5cba-4094-949a-7133a3be6509'
 
 ### Create a Team ###
