215fbb704541309b8e29a3a78d492886daf0c194
Knox Hutchinson <knox@knoxsdata.com>
2019-09-20 15:18:37 -0500

    sdwan powershell

---
+++ Powershell/Networking/sdwan.ps1
@@ -0,0 +1,30 @@
+$url = 'https://sandboxsdwan.cisco.com:8443/j_security_check'
+$login_body = @{
+    j_username = 'devnetuser'
+    j_password = 'Cisco123!'
+}
+
+$headers = @{'Accept' = 'application/json' }
+
+$response = Invoke-RestMethod -Uri $url `
+    -Method post `
+    -Body ($login_body) `
+    -ContentType 'application/x-www-form-urlencoded' `
+    -Headers $headers `
+    -SkipCertificateCheck `
+    -SessionVariable s
+$response
+
+$uri = 'https://sandboxsdwan.cisco.com:8443/dataservice/device'
+$devs = Invoke-RestMethod -Uri $uri `
+    -Method Get `
+    -ContentType 'application/json' `
+    -Headers $headers `
+    -SkipCertificateCheck `
+    -Websession $s
+
+Write-Host "Output!!!!!!!" 
+$devices = $devs.data
+ForEach ($device in $devices) {
+    write-host $device
+}
