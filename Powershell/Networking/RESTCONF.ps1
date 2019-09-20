a2dc4fdddb9e4ce2126310d6480c01f2b87304e4
Knox Hutchinson <knox@knoxsdata.com>
2019-09-20 09:39:00 -0500

    vs code ps core setup

--- Powershell/Networking/RESTCONF.ps1
+++ Powershell/Networking/RESTCONF.ps1
@@ -1,11 +1,13 @@
 $uri = 'https://ios-xe-mgmt.cisco.com:9443/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces/interface=GigabitEthernet1'
-$creds = Get-Credential #root D_Vay!_10&
+# $creds = Get-Credential #root D_Vay!_10&
+$password = ConvertTo-SecureString 'D_Vay!_10&' -AsPlainText -Force
+$Cred = New-Object System.Management.Automation.PSCredential ('root', $password)
 $headers = @{'Accept' = 'application/yang-data+json' }
 
 $response = Invoke-RestMethod -Uri $uri `
     -Method Get `
     -Authentication Basic `
-    -Credential $creds `
+    -Credential $Cred `
     -ContentType 'application/yang-data+json' `
     -Headers $headers `
     -SkipCertificateCheck
