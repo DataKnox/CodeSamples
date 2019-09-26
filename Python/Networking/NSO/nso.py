f1fea4d78bc6ac03a85bd618f2f984599b98d779
Knox Hutchinson <knox@knoxsdata.com>
2019-09-26 15:04:34 -0500

    nso print

--- Python/Networking/NSO/nso.py
+++ Python/Networking/NSO/nso.py
@@ -13,4 +13,10 @@ headers = {'Accept': ','.join(accept_header)}
 
 response = requests.get(
     f'{url_base}/running/devices/device', auth=auth, headers=headers).json()
-print(json.dumps(response, indent=2, sort_keys=True))
+#print(json.dumps(response, indent=2, sort_keys=True))
+devices = response['collection']['tailf-ncs:device']
+for device in devices:
+    print(f"Name: {device['name']}")
+    print(f"IP: {device['address']}")
+    print(f"SSH Port: {device['port']}")
+    print(" ")
