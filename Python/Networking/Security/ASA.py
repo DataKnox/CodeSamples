294cf8abcffcb710ee553d884b2ff702f2a8053d
Knox Hutchinson <knox@knoxsdata.com>
2019-09-30 14:19:43 -0500

    ASA cleanup

--- Python/Networking/Security/ASA.py
+++ Python/Networking/Security/ASA.py
@@ -4,9 +4,13 @@ import json
 url = "https://172.16.1.131/api/routing/static"
 
 headers = {
-    'Authorization': "Basic Y2lzY286Y2lzY28="
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
 }
 
-get_response = requests.get(url, headers=headers, verify=False).json()['items']
+userpw = ('cisco', 'cisco')
+
+get_response = requests.get(
+    url, headers=headers, auth=userpw, verify=False).json()['items']
 
 print(json.dumps(get_response, indent=2, sort_keys=True))
