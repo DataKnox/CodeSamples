fc0ec1b54f7f74542f209e7b83de721dad4ca290
Knox Hutchinson <knox@knoxsdata.com>
2019-09-30 13:01:42 -0500

    meraki py and posh updates

--- Powershell/Networking/meraki.ps1
+++ Powershell/Networking/meraki.ps1
@@ -4,9 +4,34 @@ $headers = @{
     'X-Cisco-Meraki-API-Key' = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
 }
 
-$response = Invoke-RestMethod -Uri $url `
+$orgs = Invoke-RestMethod -Uri $url `
     -Method get `
     -ContentType 'application/json' `
-    -Headers $headers `
+    -Headers $headers 
 
-$response | ConvertTo-Json | Write-Output
+ForEach ($org in $orgs) {
+    If ($org.name -eq 'DevNet Sandbox') {
+        $orgId = $org.id
+    }
+}
+
+$net_url = "https://dashboard.meraki.com/api/v0/organizations/$($orgId)/networks"
+$networks = Invoke-RestMethod -Uri $net_url `
+    -Method get `
+    -ContentType 'application/json' `
+    -Headers $headers 
+
+ForEach ($network in $networks) {
+    #Write-Host $network.name
+    If ($network.name -eq 'DNSMB2') {
+        $netId = $network.id
+    }
+}
+
+$devices_url = "https://dashboard.meraki.com/api/v0/networks/$($netId)/devices"
+$devices = Invoke-RestMethod -Uri $devices_url `
+    -Method get `
+    -ContentType 'application/json' `
+    -Headers $headers 
+
+$devices | ConvertTo-Json | Write-Output
