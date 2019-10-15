d46c08f6d5d74fad15f6686c394c8fe8f51d76ae
Knox Hutchinson <knox@knoxsdata.com>
2019-10-15 07:31:03 -0500

    nso changes

--- Python/Networking/NSO/nso.py
+++ Python/Networking/NSO/nso.py
@@ -5,20 +5,19 @@ import json
 url_base = 'http://10.10.21.32:8080/api'
 auth = ("admin", "admin")
 
-# List of accept headers required by NSO
-accept_header = [
-    'application/vnd.yang.api+json',
-    'application/vnd.yang.datastore+json',
-    'application/vnd.yang.data+json',
-    'application/vnd.yang.collection+json'
-]
-# Join the headers together with comma
-headers = {'Accept': ','.join(accept_header)}
+# Other useful headers
+
+#    'application/vnd.yang.api+json',
+#    'application/vnd.yang.datastore+json',
+#    'application/vnd.yang.data+json',
+
+
+headers = {'Accept': 'application/vnd.yang.collection+json'}
 
 # Get request to NSO
 response = requests.get(
     f'{url_base}/running/devices/device', auth=auth, headers=headers).json()
-#print(json.dumps(response, indent=2, sort_keys=True))
+# print(json.dumps(response, indent=2, sort_keys=True))
 
 # Parse out devices from response body
 devices = response['collection']['tailf-ncs:device']
@@ -26,4 +25,6 @@ for device in devices:
     print(f"Name: {device['name']}")
     print(f"IP: {device['address']}")
     print(f"SSH Port: {device['port']}")
+    print(f"SSH Port: {device['authgroup']}")
+    # print(device)
     print(" ")
