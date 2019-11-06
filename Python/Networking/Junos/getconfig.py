cb6e16083ce437ed0dd975afa91d13435db1b208
Knox Hutchinson <knox@knoxsdata.com>
2019-11-06 15:07:27 -0600

    added switches loop

--- Python/Networking/Junos/getconfig.py
+++ Python/Networking/Junos/getconfig.py
@@ -3,21 +3,27 @@ from pprint import pprint
 from ncclient.xml_ import *
 
 # set device configs
-switch = {"host": "10.10.10.150", "port": "830",
-          "username": "root", "password": ""}
-
-# make initial connection and save session as variable m
-with manager.connect(host=switch["host"], port=switch["port"], username=switch["username"], password=switch["password"], hostkey_verify=False, device_params={"name": "junos"}) as m:
-    # retrieve config in XML format
-    response = m.get_configuration(format='xml')
-
-    interfaces = response.xpath('configuration/interfaces/interface')
-
-    for interface in interfaces:
-        int_name = interface.xpath('name')[0].text
-        int_unit = interface.xpath('unit/name')[0].text
-        ip = []
-        for name in interface.xpath('unit/family/inet/address/name'):
-            ip.append(name.text)
-        print(f"{int_name}.{int_unit} {ip}")
-        # print(response)
+switches = [
+    {"host": "10.10.10.150", "port": "830",
+             "username": "root", "password": "S0lut1ons"},
+    {"host": "10.10.10.151", "port": "830",
+             "username": "root", "password": "S0lut1ons"}
+]
+for switch in switches:
+    # make initial connection and save session as variable m
+    with manager.connect(host=switch["host"], port=switch["port"], username=switch["username"], password=switch["password"], hostkey_verify=False, device_params={"name": "junos"}) as m:
+        # retrieve config in XML format
+        response = m.get_configuration(format='xml')
+        host = response.xpath('configuration/system/host-name')[0].text
+        interfaces = response.xpath('configuration/interfaces/interface')
+        print('='*15)
+        print(host)
+        print('='*15)
+        for interface in interfaces:
+            int_name = interface.xpath('name')[0].text
+            int_unit = interface.xpath('unit/name')[0].text
+            ip = []
+            for name in interface.xpath('unit/family/inet/address/name'):
+                ip.append(name.text)
+            print(f"{int_name}.{int_unit} {ip}")
+            # print(response)
