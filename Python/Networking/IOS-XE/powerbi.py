8cb6a9565d242f951e3f34c15e06042e4e88ed58
Knox Hutchinson <knox@knoxsdata.com>
2019-12-27 09:02:03 -0600

    add powerbi to IOSXE

---
+++ Python/Networking/IOS-XE/powerbi.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from datetime import datetime
+
+# Router IOS-XE DevNet Sandbox
+
+router = {
+    'host': 'ios-xe-mgmt.cisco.com',
+    'port': '9443',
+    'user': 'developer',
+    'pw': 'C1sco12345'
+}
+
+# Headers
+headers = {
+    'Content-Type': 'application/yang-data+json',
+    'Accept': 'application/yang-data+json'
+}
+
+url = f"https://{router['host']}:{router['port']}/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces"
+
+response = requests.get(url=url, headers=headers, auth=(
+    router['user'], router['pw']), verify=False).json()
+interfaces = response['Cisco-IOS-XE-interfaces-oper:interfaces']['interface']
+for interface in interfaces:
+    #print(json.dumps(interface, indent=2))
+    payload = []
+    body = {
+        'int-name': interface['name'],
+        'in-packets': int(interface['statistics']['in-unicast-pkts']),
+        'out-packets': int(interface['statistics']['out-unicast-pkts']),
+        'datetime': datetime.now().isoformat()
+    }
+    payload.append(body)
+    pbi_url = R"PBI URL HERE"
+    requests.post(url=pbi_url, data=json.dumps(payload))
