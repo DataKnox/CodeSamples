1d78ff474fe0818ae1c169c942e9403372ecc40c
Knox Hutchinson <knox@knoxsdata.com>
2020-01-30 21:39:18 -0600

    commit gitignore vs code setting

--- Python/Networking/Meraki/getclients.py
+++ Python/Networking/Meraki/getclients.py
@@ -28,9 +28,9 @@ net_url = f'{url}/{orgId}/networks'
 networks = requests.get(net_url, headers=headers).json()
 print(networks)
 for network in networks:
-    if network['name'] == 'DevNet Sandbox Always on READ ONLY':
+    if network['name'] == 'DNSMB3':
         netId = network['id']
 
-client_url = f'https://dashboard.meraki.com/api/v0/networks/{netId}/clients'
+client_url = f'https://dashboard.meraki.com/api/v0/networks/{netId}/l3FirewallRules'
 clients = requests.get(client_url, headers=headers).json()
 print(json.dumps(clients, indent=2, sort_keys=True))
