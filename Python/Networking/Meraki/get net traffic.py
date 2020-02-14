fcd8006852fd75b1cfd5aa0abc7eaac97de275ae
Knox Hutchinson <knox@knoxsdata.com>
2020-02-13 21:01:02 -0600

    add get network traffic

---
+++ Python/Networking/Meraki/get net traffic.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from datetime import datetime
+
+url = "https://dashboard.meraki.com/api/v0/organizations"
+
+headers = {
+    'X-Cisco-Meraki-API-Key': "6bec40cf957de430a6f1f2baa056b99a4fac9ea0",
+    'User-Agent': "PostmanRuntime/7.16.3",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "67a3f4c9-bcb4-43a5-bcde-c05ec3e976af,b27f7dd9-3ebc-4d17-a0a8-d62ab5cafb99",
+    'Accept-Encoding': "gzip, deflate",
+    'Referer': "https://api.meraki.com/api/v0/organizations",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+}
+
+response = requests.get(url, headers=headers).json()
+
+#print(json.dumps(response, indent=2, sort_keys=True))
+
+for response_org in response:
+    if response_org['name'] == 'DevNet Sandbox':
+        orgId = response_org['id']
+
+net_url = f'{url}/{orgId}/networks'
+
+networks = requests.get(net_url, headers=headers).json()
+print(networks)
+for network in networks:
+    if network['name'] == 'DNSMB5':
+        netId = network['id']
+
+client_url = f'https://dashboard.meraki.com/api/v0/networks/{netId}/traffic?timespan=604800'
+clients = requests.get(client_url, headers=headers).json()
+print(json.dumps(clients, indent=2, sort_keys=True))
