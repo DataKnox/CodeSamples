07aaa9584ae7c9c54a42d2f92b2ce1a274e4b18e
Knox Hutchinson <knox@knoxsdata.com>
2020-03-13 20:23:05 -0500

    add ENAUTO dashboard API scripts

---
+++ Python/Networking/Meraki/ENAUTOmerakiCRUD.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from requests.exceptions import HTTPError
+
+merakikey = "17d06f8fd988574af1d3280be9bdc9216134524a"
+base_url = 'https://api.meraki.com/api/v0'
+endpoint = '/organizations'
+
+headers = {
+    'X-Cisco-Meraki-API-Key': merakikey,
+    'Content-Type': 'application/json'
+}
+
+try:
+    response = requests.get(url=f"{base_url}{endpoint}", headers=headers)
+    if response.status_code == 200:
+        orgs = response.json()
+        for org in orgs:
+            if org['name'] == 'Knoxs Data':
+                orgid = org['id']
+except Exception as ex:
+    print(ex)
+
+endpoint = f"/organizations/{orgid}/networks"
+
+payload = {
+    'name': 'My Demo Net',
+    'type': 'appliance switch camera'
+}
+
+try:
+    response = requests.post(
+        url=f"{base_url}{endpoint}", headers=headers, data=json.dumps(payload))
+    if response.status_code == 201:
+        print(response.text)
+except Exception as ex:
+    print(ex)
