ab82980ea4b8e47336ec1439266f127e18e26d5a
Knox Hutchinson <knox@knoxsdata.com>
2020-04-05 20:10:24 -0500

    add cim

--- Powershell/Powershell7/OS/cim.ps1
+++ Powershell/Powershell7/OS/cim.ps1
@@ -1,1 +1,37 @@
-Get-CimInstance -Namespace root
+#Get-cimclass 
+Get-cimclass | Where-Object CimClassName -like "*network*"
+
+# Query a class
+Get-CimInstance -ClassName "CIM_NetworkAdapter"
+
+#Store a specific object in a variable
+$adapter = Get-CimInstance -ClassName "CIM_NetworkAdapter" | Where-Object Name -like "*Realtek*"
+
+$adapter
+# Get config info 
+Get-CimInstance -ClassName "Win32_NetworkAdapterConfiguration"
+
+#Get OS info
+Get-CimInstance -ClassName Win32_OperatingSystem 
+#Wait a minute, theres got to be more to our OS than that!
+Get-CimInstance -ClassName Win32_OperatingSystem | Get-Member
+Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object *
+# Does it work with NICs too?
+Get-CimInstance -ClassName "Win32_NetworkAdapterConfiguration" | Select-Object * 
+Get-CimInstance -ClassName "Win32_NetworkAdapterConfiguration" | Select-Object * | Where-Object { $_.DefaultIPGateway -ne $Null }
+
+#Common CIM classes
+<#
+Win32_Baseboard    
+Win32_BIOS
+Win32_ComputerSystem
+Win32_LogicalDisk  
+Win32_OperatingSystem  
+Win32_PingStatus
+Win32_Printer
+Win32_PrinterShare
+Win32_PhysicalMemory
+Win32_Process
+Win32_Processor    
+Win32_Share        
+#>
