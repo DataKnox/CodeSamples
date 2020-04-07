b4b68d2dfc4b460615d06a2b813caafe06ed7564
Knox Hutchinson <knox@knoxsdata.com>
2020-04-06 20:20:55 -0500

    azure ps1

---
+++ Powershell/Powershell7/Azure/connect.ps1
@@ -0,0 +1,11 @@
+#Connect-AzAccount
+
+# Get-AzSubscription 
+
+# Get-AzSubscription | Where-Object { $_.Name -eq "Knoxs" } | Select-AzSubscription
+
+Get-AzResourceGroup
+
+$rg = Get-AzResourceGroup -Name "knoxsdata"
+
+Get-AzResource -ResourceGroupName $rg.ResourceGroupName
