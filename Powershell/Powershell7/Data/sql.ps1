8ed47ba411a0d4a0ec83e0c983981d04b22ccb93
Knox Hutchinson <knox@knoxsdata.com>
2020-04-11 20:27:54 -0500

    update sql cmdlet

--- Powershell/Powershell7/Data/sql.ps1
+++ Powershell/Powershell7/Data/sql.ps1
@@ -5,6 +5,16 @@ else {
     Write-Host "Module exists!"
 }
 
+$query = @"
+SELECT 
+    *
+FROM
+    dbo.people p
+JOIN
+    dbo.planets pp 
+    ON p.HomePlanetID = pp.PlanetID
+"@
+
 Invoke-Sqlcmd -ServerInstance "localhost,1402" -Database "SWDB" -Query "SELECT * FROM dbo.People" -Username "SA" -Password "MyStrongPw!23"
 
 #Invoke-Sqlcmd -ServerInstance "localhost,1402" -Database "SWDB" -Query "SELECT * FROM dbo.People" -Username "SA" -Password "MyStrongPw!23" | Export-Excel -Path '/home/knox/Desktop/query.xlsx'
