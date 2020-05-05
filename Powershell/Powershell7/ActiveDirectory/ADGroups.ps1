9a949e50a29d84cb9f71134b536312fd4f5e117b
Knox Hutchinson <knox@knoxsdata.com>
2020-05-05 10:50:30 -0500

    adgroups

--- Powershell/Powershell7/ActiveDirectory/ADGroups.ps1
+++ Powershell/Powershell7/ActiveDirectory/ADGroups.ps1
@@ -1,11 +1,13 @@
-
-
 # Get Groups
+Get-AdGroup -Filter *
+Get-ADGroup -Filter 'GroupCategory -eq "Security" -and GroupScope -ne "DomainLocal"'
 
+Get-AdGroup -Identity "Domain Admins"
 # Get Members in a group
-
+Get-AdGroup -Identity "Domain Admins" | Get-AdGroupMember
 # New OU
-
+New-ADOrganizationalUnit -Name "Groups" -Path "OU=Users,OU=Accounts,DC=nuggetlab,DC=com"
 # New Group
-
-# Add Members to group
+New-ADGroup -Name "App Admins" -SamAccountName appAdmins -GroupCategory secuirty -GroupScope Global -DisplayName "App Admins" -Path "OU=Groups,OU=Users,OU=Accounts,DC=nuggetlab,DC=com"
+# Add Members to group d
+Add-AdGroupMember -Identity appAdmins -Members candy.spoon
