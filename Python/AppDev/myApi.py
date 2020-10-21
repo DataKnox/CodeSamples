d29f1adc83574d22537a5ae54e6f9b36026239f7
KnoxHutchinson <knox@knoxsdata.com>
2020-10-21 13:15:54 -0500

    devcor api log

--- Python/AppDev/myApi.py
+++ Python/AppDev/myApi.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import json
+import os
+from routerclass import Router
 from flask import Flask, request, jsonify
+from flask.logging import create_logger
+import logging
+logging.basicConfig(filename='c:\\Code\\CodeSamples\\Python\\AppDev\\filename.log', level=logging.DEBUG,
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 app = Flask(__name__)
+LOG = create_logger(app)
+# app.debug = True
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+LOG.info(f"script directory: {script_dir}")
+LOG.info(f"DB file: {script_dir}\db.txt")
 
 
 @app.route('/')
@@ -14,46 +26,63 @@ def index():
 
 @app.route('/routers', methods=['GET'])
 def getRouter():
-    hostname = request.args.get('hostname')
-    print(hostname)
-    with open('c:\\Code\\CodeSamples\\Python\\AppDev\\db.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-        for record in records:
-            if record['hostname'] == hostname:
-                return jsonify(record)
-        return jsonify({'error': 'data not found'})
+    try:
+        hostname = request.args.get('hostname')
+        print(hostname)
+        with open(f'{script_dir}\\db.txt', 'r') as f:
+            data = f.read()
+            records = json.loads(data)
+            for record in records:
+                if record['hostname'] == hostname:
+                    LOG.info('Routers returned')
+                    return jsonify(record), 200
+    except Exception as err:
+        LOG.error(f'Error during GET {err}')
+        return jsonify({"error": err}), 401
 
 
 @app.route('/routers', methods=['POST'])
 def addRouter():
-    record = json.loads(request.data)
-    new_records = []
-    with open('c:\\Code\\CodeSamples\\Python\\AppDev\\db.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-    for r in records:
-        if r['hostname'] != record['hostname']:
-            new_records.append(r)
-    with open('c:\\Code\\CodeSamples\\Python\\AppDev\\db.txt', 'w') as f:
-        f.write(json.dumps(new_records, indent=2))
-    return jsonify(record)
+    try:
+        record = json.loads(request.data)
+        LOG.info(f'inbound record {record}')
+        with open(f'{script_dir}\\db.txt', 'r') as f:
+            data = f.read()
+            records = json.loads(data)
+        if record in records:
+            return jsonify({"status": "Device already exists"}), 200
+        if record not in records:
+            records.append(record)
+            LOG.info(f"records output {records}")
+            LOG.warning(f'router added {record["hostname"]}')
+        with open(f'{script_dir}\\db.txt', 'w') as f:
+            f.write(json.dumps(records, indent=2))
+        return jsonify(record), 201
+    except Exception as err:
+        LOG.error(f'Error during ADD {err}')
+        return jsonify({"error": err})
 
 
 @app.route('/routers', methods=['DELETE'])
-def delteRouter():
-    record = json.loads(request.data)
-    new_records = []
-    with open('c:\\Code\\CodeSamples\\Python\\AppDev\\db.txt', 'r') as f:
-        data = f.read()
-        records = json.loads(data)
-        for r in records:
-            if r['hostname'] == record['hostname']:
-                continue
-            new_records.append(r)
-    with open('c:\\Code\\CodeSamples\\Python\\AppDev\\db.txt', 'w') as f:
-        f.write(json.dumps(new_records, indent=2))
-    return jsonify(record)
+def deleteRouter():
+    try:
+        record = json.loads(request.data)
+        new_records = []
+        with open(f'{script_dir}\\db.txt', 'r') as f:
+            data = f.read()
+            records = json.loads(data)
+            for r in records:
+                if r['hostname'] == record['hostname']:
+                    LOG.warning(f'Deleted {r["hostname"]}')
+                    continue
+                new_records.append(r)
+        with open(f'{script_dir}\\db.txt', 'w') as f:
+            f.write(json.dumps(new_records, indent=2))
+        return jsonify(record), 204
+    except Exception as err:
+        LOG.error(f'ERROR RAISED: {err}')
+        return jsonify({"error": err})
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
