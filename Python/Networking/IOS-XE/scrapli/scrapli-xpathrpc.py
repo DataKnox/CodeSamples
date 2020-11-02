bf730cd996704ca769e2cd907cc2b660e6a67d1c
DataKnox <knox@knoxsdata.com>
2020-11-01 20:56:19 -0600

    update w query

--- Python/Networking/IOS-XE/scrapli/scrapli-xpathrpc.py
+++ Python/Networking/IOS-XE/scrapli/scrapli-xpathrpc.py
@@ -1,12 +1,12 @@
 from scrapli_netconf.driver import NetconfScrape
 from mydevice import *
 
-# import logging
-# logging.basicConfig(level=logging.DEBUG)
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 xpath_filter = '''
 <get>
-<filter xmlns:t="http://cisco.com/ns/yang/Cisco-IOS-XE-ospf-oper" type="xpath" select="/ospf-oper-data" />
+<filter xmlns:t="http://cisco.com/ns/yang/Cisco-IOS-XE-ospf-oper" type="xpath" select="/ospf-oper-data/ospf-state/ospf-instance[af='address-family-ipv4' and router-id='168432911']" />
 </get>
 '''
 
