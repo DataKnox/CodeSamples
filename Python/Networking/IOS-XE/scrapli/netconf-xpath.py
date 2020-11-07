1ee77e60cd4b4c276119e19d31dbdcd339c17c1a
DataKnox <knox@knoxsdata.com>
2020-11-06 21:56:02 -0600

    nornir netconf get

--- Python/Networking/IOS-XE/scrapli/netconf-xpath.py
+++ Python/Networking/IOS-XE/scrapli/netconf-xpath.py
@@ -6,9 +6,10 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 eigrp_filter = '/eigrp-oper-data/eigrp-instance/eigrp-topo/eigrp-network[afi="eigrp-af-ipv4" and ip-prefix="192.168.10.0/24"]/rd-vecmetric'
+ospf_filter = '/ospf-oper-data'
 
-conn = NetconfScrape(**switch)
+conn = NetconfScrape(**router)
 conn.open()
 response = conn.get(
-    filter_=eigrp_filter, filter_type='xpath',)
+    filter_=ospf_filter, filter_type='xpath',)
 print(response.result)
--- Python/Networking/Nornir/helloworld.py
+++ Python/Networking/Nornir/helloworld.py
@@ -1,10 +1,35 @@
 from nornir import InitNornir
 from nornir_scrapli.tasks import netconf_get
+from nornir_utils.plugins.functions import print_result
+import xmltodict
+import json
 import os
+
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
-nr = InitNornir(
-    config_file=f"{script_dir}\config.yml")
-ospf_xpath = '/ospf-oper-data/ospf-state/ospf-instance[af="address-family-ipv4" and router-id="16843009"]/ospf-area[area-id=0]/ospf-interface[name="GigabitEthernet2"]/ospf-neighbor[neighbor-id="2.2.2.2"]/state'
-result = nr.run(task=netconf_get, filter_type='xpath', filter_=ospf_xpath)
-print(result)
+nr = InitNornir(config_file=f"{script_dir}/config.yml")
+
+
+ospf_xpath = '/ospf-oper-data/ospf-state/ospf-instance'
+node_data = {}
+
+results = nr.run(task=netconf_get, filter_type='xpath', filter_=ospf_xpath)
+# Get address family and router-id
+for result in results.keys():
+    data = xmltodict.parse(results[result].result)
+    # print(data)
+    node_data[result] = {}
+    node_data[result]['af'] = data['rpc-reply']['data']['ospf-oper-data']['ospf-state']['ospf-instance']['af']
+    node_data[result]['router-id'] = data['rpc-reply']['data']['ospf-oper-data']['ospf-state']['ospf-instance']['router-id']
+    node_data[result]['area-id'] = data['rpc-reply']['data']['ospf-oper-data']['ospf-state']['ospf-instance']['ospf-area']['area-id']
+    for ints in data['rpc-reply']['data']['ospf-oper-data']['ospf-state']['ospf-instance']['ospf-area']['ospf-interface']:
+        if 'ospf-neighbor' in ints:
+            node_data[result]['if-name'] = ints['name']
+            node_data[result]['neighbor-id'] = ints['ospf-neighbor']['neighbor-id']
+
+for result in results.keys():
+    if result in node_data:
+        ospf_xpath = f'/ospf-oper-data/ospf-state/ospf-instance[af="{node_data[result]["af"]}" and router-id="{node_data[result]["router-id"]}"]/ospf-area[area-id={node_data[result]["area-id"]}]/ospf-interface[name="{node_data[result]["if-name"]}"]/ospf-neighbor[neighbor-id="{node_data[result]["neighbor-id"]}"]/state'
+        result = nr.run(task=netconf_get, filter_type='xpath',
+                        filter_=ospf_xpath)
+        print_result(result)
