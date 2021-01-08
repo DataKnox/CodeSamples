9ad2c0346394738e42d3b48b64cd4da540b18a7e
Adrian Roata <adi.roata@gmail.com>
2021-01-09 00:06:17 +0200

    Update netconf-1.py
    # updated host address and port with currently running DevNet Sandbox
    # added new arguments to eliminate 'ncclient.transport.errors.AuthenticationError: SSHException('No existing session')' error.

--- Python/Networking/IOS-XE/netconf-1.py
+++ Python/Networking/IOS-XE/netconf-1.py
@@ -1,8 +1,9 @@
 from ncclient import manager
 
-
-router = {"host": "ios-xe-mgmt-latest.cisco.com", "port": "10000",
+# updated host address and port
+router = {""host": "sandbox-iosxe-latest-1.cisco.com", "port": "830",
           "username": "developer", "password": "C1sco12345"}
 
-with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
+# added new arguments to eliminate 'ncclient.transport.errors.AuthenticationError: SSHException('No existing session')' error.
+with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False, allow_agent=False, look_for_keys=False) as m:
     m.close_session()
